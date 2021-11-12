21bd41beb53840b9c3e05f6b8f8f10b7760aa238
ImBestBoy <Mohamadsajad2012@gmail.com>
2021-11-12 12:55:50 +0330

    add post section and initialized

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("account.urls")),
+    path("", include("post.urls")),
 ]
 
 if settings.DEBUG:
